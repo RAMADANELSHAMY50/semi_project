adf66d8996cdc011cad3351babbc0ad068cb4028
Ramadan Elshamy <elshamyramadan203@gmail.com>
2026-05-09 21:06:18 +0300

    update on navbar and some pages

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,3 +1,39 @@
+let navLinksAll = document.querySelectorAll(".nav-link");
+
+navLinksAll.forEach(link => {
+
+    link.addEventListener("click", () => {
+
+        navLinksAll.forEach(item => {
+            item.classList.remove("active");
+        });
+
+        link.classList.add("active");
+
+    });
+
+});
+
+const menu = document.getElementById("menu");
+const links = document.querySelector(".links");
+// const navLinksAll = document.querySelectorAll(".nav-link");
+
+menu.addEventListener("click", () => {
+    links.classList.toggle("show");
+});
+
+
+
+navLinksAll.forEach(link => {
+
+    link.addEventListener("click", () => {
+        links.classList.remove("show");
+    });
+
+});
+
+
+
 document.querySelectorAll(".question").forEach((q) => {
     q.addEventListener("click", () => {
         const answer = q.nextElementSibling;
@@ -10,4 +46,26 @@ document.querySelectorAll(".question").forEach((q) => {
         plus.classList.toggle("d-none");
         minus.classList.toggle("d-none");
     });
+});
+
+document.querySelectorAll(".btns").forEach(btn => {
+
+    btn.addEventListener("click", () => {
+
+        let season = btn.closest(".Season");
+
+        let arrowDown = btn.querySelector(".arrow-down");
+        let arrowUp = btn.querySelector(".arrow-up");
+
+        let seasonBoxes = season.querySelectorAll(".Seaason-box");
+
+        arrowDown.classList.toggle("d-none");
+        arrowUp.classList.toggle("d-none");
+
+        seasonBoxes.forEach(box => {
+            box.classList.toggle("d-none");
+        });
+
+    });
+
 });
